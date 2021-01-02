8c33067ce7b93b977dcf409d520fa4b32bcea0bf
kouheikishigami <kouhei19920522@gmail.com>
2021-01-02 15:33:37 +0900

    first commit

---
+++ packages/backend/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  entry: './functions/src/index.ts',
+  output: {
+    filename: 'index.js',
+    path: `${__dirname}/lib`,
+    libraryTarget: 'this',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json', '.test.ts'],
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  },
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+        options: {
+          configFile: path.resolve(__dirname, 'tsconfig.json'),
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'production',
+    }),
+  ],
+  externals: [nodeExternals()],
+};
