c4dadeb0ea45540fc236a0b58f9f3f1550de76a8
kouheikishigami <kouhei19920522@gmail.com>
2021-01-10 02:18:07 +0900

    configとenvの設定

--- packages/backend/functions/v1/request/generateToken.ts
+++ packages/backend/functions/v1/request/generateToken.ts
@@ -7,12 +7,22 @@ export const generateToken = functions.https.onRequest(async (req, res) => {
     return;
   }
 
-  const appID = '970CA35de60c44645bbae8a215061b33';
-  const appCertificate = '5CFd2fd1755d40ecb72977518be15d3b';
-  const channelName = '7d72365eb983485397e3e3f9d460bdda';
-  const uid = 2882341273;
-  const role = Role.PUBLISHER;
+  const appID = functions.config()?.env?.appid;
+  const appCertificate = functions.config()?.env?.certificate;
+  const channelName = req.query?.channelName;
+  const uid = req.query?.uid;
 
+  if (!appID || !appCertificate) {
+    res.status(400).send('config not set');
+    return;
+  }
+
+  if (!channelName || !uid) {
+    res.status(400).send('Query parameters not set');
+    return;
+  }
+
+  const role = Role.PUBLISHER;
   const expirationTimeInSeconds = 3600;
   const currentTimestamp = Math.floor(Date.now() / 1000);
   const privilegeExpiredTs = currentTimestamp + expirationTimeInSeconds;
@@ -20,8 +30,8 @@ export const generateToken = functions.https.onRequest(async (req, res) => {
   const tokenA = await RtcTokenBuilder.buildTokenWithUid(
     appID,
     appCertificate,
-    channelName,
-    uid,
+    channelName as string,
+    Number(uid as string),
     role,
     privilegeExpiredTs,
   );
